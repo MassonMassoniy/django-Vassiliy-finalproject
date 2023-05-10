353aab48956cc6f83efc2ce18cdce27c707b65d8
MassonMassoniy <ageev.vassya.2@gmail.com>
2023-05-10 19:51:05 +0600

    post page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, about, post_single
+from .views import index, about, post_single, post_form
 
 urlpatterns=[
     path('',index),
     path('about/',about),
+    path('post/new/',post_form,name="post_form"),
     path('<int:pk>/',post_single,name='single')
 ]
