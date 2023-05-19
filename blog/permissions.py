55d97e1c318f63c24803e9cafa9e529ed8711df6
MassonMassoniy <ageev.vassya.2@gmail.com>
2023-05-19 22:51:02 +0600

    role checker update

--- blog/permissions.py
+++ blog/permissions.py
@@ -1,7 +1,29 @@
 from rest_framework.permissions import BasePermission
 from .models import User
 
+'--------------------------------------------------------------------------------------------------'
+
 class IsCourier(BasePermission):
 
     def has_permission(self, request, view):
         return request.user.role == User.COURIER
+    
+class IsSeller(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.role == User.SELLER
+    
+class IsAssembler(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.role == User.ASSEMBLER
+    
+class IsCustomer(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.role == User.CUSTOMER
+    
+class IsAdmin(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.role == User.ADMIN
--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import *
 from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from rest_framework.urlpatterns import format_suffix_patterns
+#from rest_framework.urlpatterns import format_suffix_patterns
 
 router = DefaultRouter()
 router.register('post', PostView, basename = 'post')
@@ -12,12 +12,21 @@ urlpatterns=[
     path('about/',about),
     path('post/new/',post_form,name="post_form"),
     path('<int:pk>/',post_single,name='single'),
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/', include(router.urls)),
+
     path('token/',TokenObtainPairView.as_view()),
     path('token/refresh/',TokenRefreshView.as_view()),
+
     path('register/',RegisterView.as_view({'post':'create'})),
     path('user/me/',UserView.as_view({'get':'get_current_user'})),
+
+    path('courier/get/', CourierView.as_view()),
+    path('seller/get/', SellerView.as_view()),
+    path('assembler/get/', AssemblerView.as_view()),
+    path('customer/get/', CustomerView.as_view()),
+    path('administrator/get/', AdminView.as_view()),
 ]
